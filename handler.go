edfcd3bc8a38ceb5780ddc0a5f4c73d5759c7d8f
Arya Hadi <arya@cafebazaar.ir>
2018-02-01 23:42:26 +0330

    Fix message callbacks

--- handler.go
+++ handler.go
@@ -52,7 +52,7 @@ func (b *Bot) handleNewUpdate(update *telegramAPI.Update) {
 				userSession.messageCallback(userSession, update.Message.Text)
 			} else {
 				for _, callback := range callbacks {
-					if matches := callback.Pattern.FindStringSubmatch(update.Message.Command()); matches != nil {
+					if matches := callback.Pattern.FindStringSubmatch(update.Message.Text); matches != nil {
 						callback.Function(userSession, matches)
 						break
 					}
